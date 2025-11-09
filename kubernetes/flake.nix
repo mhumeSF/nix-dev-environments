c421e775d018e52d13d90feebe85b700baae2697
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2025-11-09 13:59:03 -0500

    Adding kubernetes flake

---
+++ kubernetes/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A Nix-flake-based k8s development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+          ];
+        };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            fluxcd
+            kind
+            kubectl
+            kubectx
+            kubectl-neat
+            kubernetes-helm
+            kustomize
+            renovate
+
+            yaml-language-server
+          ];
+
+          env = {
+          };
+
+        };
+      });
+    };
+}
